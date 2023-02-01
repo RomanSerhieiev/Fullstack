b69a8c6d4777f2097b877bf44b6fc6982b6290a5
RomanSerhieiev <unique.rmn.8@gmail.com>
2023-02-01 13:38:54 +0200

    JS homework 1

---
+++ JS/homework_1/js/main.js
@@ -0,0 +1,59 @@
+// First task
+
+let hello = 'hello';
+let owu = 'owu';
+let com = 'com';
+let ua = 'ua';
+let one = 1;
+let ten = 10;
+let nnn = -999;
+let ott = 123;
+const PI = 3.14
+let tas = 2.7;
+let st = 16;
+let t = true;
+let f = false;
+
+console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(one);
+console.log(ten);
+console.log(nnn);
+console.log(ott);
+console.log(PI);
+console.log(tas);
+console.log(st);
+console.log(t);
+console.log(f);
+
+// Second task
+
+let firstName = "Roman";
+let middleName = "Serhiiovych";
+let lastName = "Serhieiev";
+let person = lastName+" "+firstName+" "+middleName;
+
+console.log(person);
+
+// Third task
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+// Fourth task
+
+let fn = prompt("My name","");
+let mn = prompt("My middle name","");
+let age = prompt("My age","");
+let mfn = "My name is";
+let mage1 = "and I'm";
+let mage2 = "years old.";
+
+console.log(`${mfn} ${fn} ${mn} ${mage1} ${age} ${mage2}`);
