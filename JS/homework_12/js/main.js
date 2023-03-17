2e565006b7db08c9cf5e48cb15e8bd040a7986db
RomanSerhieiev <unique.rmn.8@gmail.com>
2023-03-17 13:14:35 +0200

    JS homework 12.1

--- JS/homework_12/js/main.js
+++ JS/homework_12/js/main.js
@@ -13,26 +13,3 @@ fetch('https://jsonplaceholder.typicode.com/users')
             usersList.appendChild(listItem);
         });
     });
-
-const params = new URLSearchParams(window.location.search);
-const userId = params.get('id');
-
-fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
-    .then(response => response.json())
-    .then(user => {
-        document.getElementById('id').textContent = user.id;
-        document.getElementById('name').textContent = user.name;
-        document.getElementById('username').textContent = user.username;
-        document.getElementById('email').textContent = user.email;
-        document.getElementById('addressStreet').textContent = user.address.street;
-        document.getElementById('addressSuite').textContent = user.address.suite;
-        document.getElementById('addressCity').textContent = user.address.city;
-        document.getElementById('addressZipcode').textContent = user.address.zipcode;
-        document.getElementById('addressGeoLat').textContent = user.address.geo.lat;
-        document.getElementById('addressGeoLng').textContent = user.address.geo.lng;
-        document.getElementById('phone').textContent = user.phone;
-        document.getElementById('website').textContent = user.website;
-        document.getElementById('companyName').textContent = user.company.name;
-        document.getElementById('companyCatchPhrase').textContent = user.company.catchPhrase;
-        document.getElementById('companyBs').textContent = user.company.bs;
-    });
