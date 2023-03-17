649cb9d65eedc1b5ccdec5ba136d705d604c25f1
RomanSerhieiev <unique.rmn.8@gmail.com>
2023-03-17 18:02:41 +0200

    JS mini-project 1.0

--- JS/mini-project/js/main.js
+++ JS/mini-project/js/main.js
@@ -1,29 +1,43 @@
+const container = document.getElementById('index-container');
+const userH1 = document.createElement('h1');
+userH1.innerText = `Users`;
+container.appendChild(userH1);
+
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => {
-        const container = document.getElementById('users-container'); // Отримуємо контейнер для виведення
         users.forEach(user => {
-            // Створюємо блок для користувача
             const userBlock = document.createElement('div');
             userBlock.classList.add('user-block');
 
-            // Додаємо в блок інформацію про користувача
             const userId = document.createElement('p');
-            userId.innerText = `ID: ${user.id}`;
+            const userIdSpanO = document.createElement('span');
+            userIdSpanO.classList.add('orange');
+            userIdSpanO.innerText = `ID: `;
+            const userIdSpanW = document.createElement('span');
+            userIdSpanW.innerText = `${user.id}`;
+            userId.appendChild(userIdSpanO);
+            userId.appendChild(userIdSpanW);
             userBlock.appendChild(userId);
 
             const userName = document.createElement('p');
-            userName.innerText = `Name: ${user.name}`;
+            const userNameSpanO = document.createElement('span');
+            userNameSpanO.classList.add('orange');
+            userNameSpanO.innerText = `Name: `;
+            const userNameSpanW = document.createElement('span');
+            userNameSpanW.innerText = `${user.name}`;
+            userName.appendChild(userNameSpanO);
+            userName.appendChild(userNameSpanW);
             userBlock.appendChild(userName);
 
-            // Додаємо кнопку для переходу на сторінку з детальною інформацією про користувача
             const userLink = document.createElement('a');
             userLink.href = `user-details.html?id=${user.id}`;
-            userLink.innerText = 'Details';
-            userBlock.appendChild(userLink);
 
-            // Додаємо блок з користувачем до контейнеру
+            const btn = document.createElement('button');
+            btn.innerText = 'Details';
+
+            userLink.appendChild(btn);
+            userBlock.appendChild(userLink);
             container.appendChild(userBlock);
         });
-    })
-    .catch(error => console.log(error));
+    });
